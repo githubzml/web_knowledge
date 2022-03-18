4c6522c31c757b76215215c1e4987b9befd03579
zxy_zhuminglu <15712965708@163.com>
2022-03-18 17:16:22 +0800

    package.json html js 废话

---
+++ vue/定时器.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>null</div>
+</template>
+
+<script>
+// 开启/清除 定时器
+export default {
+  data() {
+    return {
+      timer: null,
+    };
+  },
+  mounted() {
+    this.timer = setInterval(() => {
+      console.log("1");
+    }, 1000);
+  },
+  beforeDestory() {
+    clearInterval(this.timer);
+    this.timer = null;
+  },
+};
+</script>
+
+<style>
+</style>
